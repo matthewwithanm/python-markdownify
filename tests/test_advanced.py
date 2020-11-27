7685738344a27deeaa6148cd1cd117d69cb35967
Andrew Richards <ar-dev-misc@acrconsulting.co.uk>
2020-11-27 14:18:08 +0000

    Formatting tweak
    Change indent of continuation line; squashes a flake8 warning.

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -10,4 +10,4 @@ def test_code_with_tricky_content():
     assert md('<code>></code>') == "`>`"
     assert md('<code>/home/</code><b>username</b>') == "`/home/`**username**"
     assert md('First line <code>blah blah<br />blah blah</code> second line') \
-            == "First line `blah blah  \nblah blah` second line"
+        == "First line `blah blah  \nblah blah` second line"
