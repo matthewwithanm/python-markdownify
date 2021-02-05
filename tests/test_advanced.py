457454c713ce03752ca96309a796e66275edc9dd
Bruno Miguens <brunomiguens@icloud.com>
2021-02-05 19:41:43 +0000

    Add new line at the end of file

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -5,9 +5,11 @@ def test_nested():
     text = md('<p>This is an <a href="http://example.com/">example link</a>.</p>')
     assert text == 'This is an [example link](http://example.com/).\n\n'
 
+
 def test_ignore_comments():
     text = md("<!-- This is a comment -->")
     assert text == ""
+
 
 def test_ignore_comments_with_other_tags():
     text = md("<!-- This is a comment --><a href='http://example.com/'>example link</a>")
