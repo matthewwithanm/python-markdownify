a152c5b7068bab9289e1650f1ca6c280011c8a2a
Bruno Miguens <brunomiguens@icloud.com>
2021-02-08 19:32:35 +0000

    Fix lint

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -26,17 +26,17 @@ table = re.sub(r'\s+', '', """
 <table>
     <tr>
         <th>Firstname</th>
-        <th>Lastname</th> 
+        <th>Lastname</th>
         <th>Age</th>
     </tr>
     <tr>
         <td>Jill</td>
-        <td>Smith</td> 
+        <td>Smith</td>
         <td>50</td>
     </tr>
     <tr>
         <td>Eve</td>
-        <td>Jackson</td> 
+        <td>Jackson</td>
         <td>94</td>
     </tr>
 </table>
@@ -55,12 +55,12 @@ table_head_body = re.sub(r'\s+', '', """
     <tbody>
         <tr>
             <td>Jill</td>
-            <td>Smith</td> 
+            <td>Smith</td>
             <td>50</td>
         </tr>
         <tr>
             <td>Eve</td>
-            <td>Jackson</td> 
+            <td>Jackson</td>
             <td>94</td>
         </tr>
     </tbody>
