ee53b6f8c5324ba69fe47c5fe3634398a1318b75
Adam Bambuch <adam.bambuch2@gmail.com>
2022-07-21 22:25:57 +0200

    don't escape text in pre tag (Fenced Code Blocks) - test

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -187,6 +187,7 @@ def test_p():
 def test_pre():
     assert md('<pre>test\n    foo\nbar</pre>') == '\n```\ntest\n    foo\nbar\n```\n'
     assert md('<pre><code>test\n    foo\nbar</code></pre>') == '\n```\ntest\n    foo\nbar\n```\n'
+    assert md('<pre>this_should_not_escape</pre>') == '\n```\nthis_should_not_escape\n```\n'
 
 
 def test_s():
