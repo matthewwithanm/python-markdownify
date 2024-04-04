abee80626768cb679515c9c580de973f8b37ed0b
AlexVonB <AlexVonB@users.noreply.github.com>
2024-04-04 21:37:04 +0200

    replace loop for single regex for substitution

--- markdownify/__init__.py
+++ markdownify/__init__.py
@@ -201,8 +201,7 @@ class MarkdownConverter(object):
         if not text:
             return ''
         if self.options['escape_misc']:
-            for c in '\\&<`[>~#=+-|':
-                text = text.replace(c, '\\' + c)
+            text = re.sub(r'([\\&<`[>~#=+|-])', r'\\\1', text)
             text = re.sub(r'([0-9])([.)])', r'\1\\\2', text)
         if self.options['escape_asterisks']:
             text = text.replace('*', r'\*')
