5783c8a93b72d232b21bf3d7da7aa676306ef763
itmammoth <itmammoth@gmail.com>
2025-02-08 13:30:15 +0900

    Support `video` tag with `poster` attribute

--- markdownify/__init__.py
+++ markdownify/__init__.py
@@ -463,6 +463,19 @@ class MarkdownConverter(object):
 
         return '![%s](%s%s)' % (alt, src, title_part)
 
+    def convert_video(self, el, text, convert_as_inline):
+        # `text` is a fallback for browsers that do not support video, so it's unused
+        src = el.attrs.get('src', None) or ''
+        poster = el.attrs.get('poster', None) or ''
+        if (convert_as_inline
+                and el.parent.name not in self.options['keep_inline_images_in']):
+            return ''
+        if not poster:
+            return ''
+        if src:
+            return '[![](%s)](%s)' % (poster, src)
+        return '![](%s)' % poster
+
     def convert_list(self, el, text, convert_as_inline):
 
         # Converting a list to inline is undefined.
