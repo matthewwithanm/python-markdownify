dbff386d0a41cb4a0b7bbc41d6e335ceb1054d05
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-07-14 20:45:28 +0000

    fix: do not use `Unpack` as it requires Python 3.12

--- markdownify/__init__.pyi
+++ markdownify/__init__.pyi
@@ -1,5 +1,4 @@
 from _typeshed import Incomplete
-from typing import TypedDict, Unpack
 
 ATX: str
 ATX_CLOSED: str
@@ -15,37 +14,60 @@ STRIP: str
 STRIP_ONE: str
 
 
-class Options(TypedDict, total=False):
-  autolinks: bool
-  bs4_options: str
-  bullets: str  # An iterable of bullet types.
-  code_language: str
-  code_language_callback: Incomplete | None
-  convert: list[str] | None
-  default_title: bool
-  escape_asterisks: bool
-  escape_underscores: bool
-  escape_misc: bool
-  heading_style: str
-  keep_inline_images_in: list[str]
-  newline_style: str
-  strip: list[str] | None
-  strip_document: str | None
-  strip_pre: str
-  strong_em_symbol: str
-  sub_symbol: str
-  sup_symbol: str
-  table_infer_header: bool
-  wrap: bool
-  wrap_width: int
-
-
-def markdownify(html: str, **kwargs: Unpack[Options]) -> str: ...
+def markdownify(
+    html: str,
+    autolinks: bool = ...,
+    bs4_options: str = ...,
+    bullets: str = ...,
+    code_language: str = ...,
+    code_language_callback: Incomplete | None = ...,
+    convert: list[str] | None = ...,
+    default_title: bool = ...,
+    escape_asterisks: bool = ...,
+    escape_underscores: bool = ...,
+    escape_misc: bool = ...,
+    heading_style: str = ...,
+    keep_inline_images_in: list[str] = ...,
+    newline_style: str = ...,
+    strip: list[str] | None = ...,
+    strip_document: str | None = ...,
+    strip_pre: str = ...,
+    strong_em_symbol: str = ...,
+    sub_symbol: str = ...,
+    sup_symbol: str = ...,
+    table_infer_header: bool = ...,
+    wrap: bool = ...,
+    wrap_width: int = ...,
+) -> str: ...
 
 
 class MarkdownConverter:
-  def __init__(self, **kwargs: Unpack[Options]) -> None:
-    ...
+    def __init__(
+        self,
+        autolinks: bool = ...,
+        bs4_options: str = ...,
+        bullets: str = ...,
+        code_language: str = ...,
+        code_language_callback: Incomplete | None = ...,
+        convert: list[str] | None = ...,
+        default_title: bool = ...,
+        escape_asterisks: bool = ...,
+        escape_underscores: bool = ...,
+        escape_misc: bool = ...,
+        heading_style: str = ...,
+        keep_inline_images_in: list[str] = ...,
+        newline_style: str = ...,
+        strip: list[str] | None = ...,
+        strip_document: str | None = ...,
+        strip_pre: str = ...,
+        strong_em_symbol: str = ...,
+        sub_symbol: str = ...,
+        sup_symbol: str = ...,
+        table_infer_header: bool = ...,
+        wrap: bool = ...,
+        wrap_width: int = ...,
+    ) -> None:
+        ...
   
-  def convert(self, html: str) -> str:
-    ...
+    def convert(self, html: str) -> str:
+        ...
