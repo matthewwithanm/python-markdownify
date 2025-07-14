8a991b059a4ca40ea787b8b3b7196fd0c593dc85
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-07-14 20:34:54 +0000

    feat: expand types for MarkdownConverter

--- markdownify/__init__.pyi
+++ markdownify/__init__.pyi
@@ -1,4 +1,5 @@
 from _typeshed import Incomplete
+from typing import TypedDict, Unpack
 
 ATX: str
 ATX_CLOSED: str
@@ -13,29 +14,38 @@ RSTRIP: str
 STRIP: str
 STRIP_ONE: str
 
-def markdownify(
-  html: str,
-  strip: list[str] = ...,
-  convert: list[str] = ...,
-  autolinks: bool = ...,
-  bullets: str = ...,  # An iterable of bullet types.
-  code_language: str = ...,
-  code_language_callback: Incomplete = ...,
-  default_title: bool = ...,
-  escape_asterisks: bool = ...,
-  escape_underscores: bool = ...,
-  escape_misc: bool = ...,
-  heading_style: str = ...,
-  keep_inline_images_in: list[str] = ...,
-  newline_style: str = ...,
-  strip_document: str | None = ...,
-  strong_em_symbol: str = ...,
-  sub_symbol: str = ...,
-  sup_symbol: str = ...,
-  table_infer_header: bool = ...,
-  wrap: bool = ...,
-  wrap_width: int = ...,
-) -> str: ...
+
+class Options(TypedDict, total=False):
+  autolinks: bool
+  bs4_options: str
+  bullets: str  # An iterable of bullet types.
+  code_language: str
+  code_language_callback: Incomplete | None
+  convert: list[str] | None
+  default_title: bool
+  escape_asterisks: bool
+  escape_underscores: bool
+  escape_misc: bool
+  heading_style: str
+  keep_inline_images_in: list[str]
+  newline_style: str
+  strip: list[str] | None
+  strip_document: str | None
+  strip_pre: str
+  strong_em_symbol: str
+  sub_symbol: str
+  sup_symbol: str
+  table_infer_header: bool
+  wrap: bool
+  wrap_width: int
+
+
+def markdownify(html: str, **kwargs: Unpack[Options]) -> str: ...
+
 
 class MarkdownConverter:
-  pass
+  def __init__(self, **kwargs: Unpack[Options]) -> None:
+    ...
+  
+  def convert(self, html: str) -> str:
+    ...
